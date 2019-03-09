0a9f904c824325878926cdd394508c3e06f357b0
Arnesh Regmi <arneshregmi@Arneshs-iMac.local>
2019-03-08 18:30:21 -0800

    Reloads on win and/or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+
 
 var randomNum;
 var emeraldValue = 1;
@@ -10,55 +10,57 @@ var Wins = 0;
 var Loss = 0;
 
 // Generate Random Number and set it on Screen
-function randomNumber(){
-    randomNum = Math.floor(Math.random() * 100);
-    $("#Number").attr("placeholder", randomNum);
-    console.log("number", randomNum)
-    $("#RandomNumber").text("Number: " + randomNum)
-  };
-  
+function randomNumber() {
+  randomNum = Math.floor(Math.random() * 100);
+  $("#Number").attr("placeholder", randomNum);
+  console.log("number", randomNum)
+  $("#RandomNumber").text("Number: " + randomNum)
+};
 
-  function WinOrLoss(score, num){
-    if(score > num)
-    {
-      console.log("Loss!!");
-      $("#Loss-text").text("You Lose!");
-    }
-    else if (score === num){
-      console.log("Win!!");
-      $("#Wins-text").text("You Win!");
+
+function WinOrLoss(score, num) {
+  if (score > num) {
+    console.log("Loss!!");
+    $("#Loss-text").text("You Lose!");
+    setTimeout(function () { location.reload(); }, 3000);
   }
-  $("#Total-Score").text("Your total Score:"+score);
+  else if (score === num) {
+    console.log("Win!!");
+    $("#Wins-text").text("You Win!");
+    setTimeout(function () { location.reload(); }, 3000);
+  }
+  $("#Total-Score").text("Your total Score:" + score);
+
 }
 
-  $(document).ready(function() {
-    var totalScore = 0;
+$(document).ready(function () {
+  var totalScore = 0;
 
-    // create an event litener, that run this function
-    $("#Emerald").on("click", function () {
-      totalScore = totalScore + emeraldValue;
-      console.log(totalScore);
-      WinOrLoss(totalScore, randomNum);
-    });
-    $("#Ruby").on("click", function () {
-      totalScore = totalScore + rubyValue;
-      console.log(totalScore);
-      WinOrLoss(totalScore, randomNum);
-    });
-    $("#Diamond").on("click", function () {
-      totalScore = totalScore + diamondValue;
-      console.log(totalScore);
-      WinOrLoss(totalScore, randomNum);
-    });
-    $("#Saphire").on("click", function () {
-      totalScore = totalScore + saphireValue;
-      console.log(totalScore);
-      WinOrLoss(totalScore, randomNum);
-    });
-
+  // create an event litener, that run this function
+  $("#Emerald").on("click", function () {
+    totalScore = totalScore + emeraldValue;
+    console.log(totalScore);
+    WinOrLoss(totalScore, randomNum);
   });
-  console.log("Random"+randomNum);
-  
-  randomNumber();
+  $("#Ruby").on("click", function () {
+    totalScore = totalScore + rubyValue;
+    console.log(totalScore);
+    WinOrLoss(totalScore, randomNum);
+  });
+  $("#Diamond").on("click", function () {
+    totalScore = totalScore + diamondValue;
+    console.log(totalScore);
+    WinOrLoss(totalScore, randomNum);
+  });
+  $("#Saphire").on("click", function () {
+    totalScore = totalScore + saphireValue;
+    console.log(totalScore);
+    WinOrLoss(totalScore, randomNum);
+  });
 
- 
+});
+console.log("Random" + randomNum);
+
+randomNumber();
+
+
